bb55ca55d8ac5c489361d704caf367e105bb227f
66011212038-lgtm <66011212038@msu.ac.th>
2025-10-15 03:19:59 +0700

    add History page

---
+++ lib/page/HistoryUser.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class History extends StatelessWidget {
+  const History({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+        backgroundColor: const Color(0xFFFDE10A),
+        title: const Text(
+          "ประวัติการส่งสินค้า",
+          style: TextStyle(color: Colors.black),
+        ), //textbar
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView.builder(
+          itemCount: 4, // เปลี่ยนจำนวนรายการตามต้องการ
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.only(
+                bottom: 24,
+              ), // เว้นระยะห่างแต่ละกล่อง
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey.shade300),
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    blurRadius: 6,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset('assets/images/logo.png', width: 60, height: 60),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: const [
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.red,
+                              size: 20,
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              'มหาวิทยาลัยมหาสารคาม',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text('ชื่อผู้รับ : Manlika'),
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          children: const [
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.green,
+                              size: 20,
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              'Big-C Onnut',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text('ชื่อผู้ส่ง : Mullika'),
+                        ),
+                        const SizedBox(height: 16),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                              foregroundColor: Colors.black,
+                            ),
+                            child: const Text("รายละเอียด"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
